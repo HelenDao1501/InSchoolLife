f7fedfe618fc821b760d6bfa609a7efc2a89cbf2
fight for-code <teenlonglanh@gmail.com>
2021-07-25 21:37:58 +0700

    first commit

---
+++ C#/Swap/Swap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    class Program
+    {
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+             int a = 10, b = 90;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+
+
+            String s1 = "Hello", s2 = "There";
+            Console.WriteLine("Before swap: {0}, {1}!", s1, s2);
+            Swap<string>(ref s1, ref s2);
+            Console.WriteLine("After swap: {0}, {1}!", s1, s2);
+            Console.ReadLine();
+
+        }
+
+    }
+}
